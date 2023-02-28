1c9ce20e93af0d7bb4b2970ec1c34da67bbbe9da
nicobaldu <87109111+nicobaldu@users.noreply.github.com>
2023-02-28 10:30:08 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("boton").addEventListener("click", function(){
+    document.getElementById("contacto").innerHTML = "<h4 id=js>Gracias por contactarnos! </br> En breve nos estaremos comunicando.</h4>";
+})
